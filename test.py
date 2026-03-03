da7f4558ea252e9264a5d8a1fffe3247898937f0
Parcurcik <pavelbezborodov@bk.ru>
2026-03-03 21:15:18 +0500

    add new test

--- test.py
+++ test.py
@@ -1,13 +1,8 @@
-import pytest
-
-
 def sample_add(a, b):
     return a + b
 
-
 def test_add_positive_numbers():
     assert sample_add(2, 3) == 5
-
 
 def test_add_negative_numbers():
     assert sample_add(-1, 1) == 0
